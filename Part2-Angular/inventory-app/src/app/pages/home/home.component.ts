32a69414f433122af313a89b6c730c1e0c31d350
H1PP0-G <3328272733@qq.com>
2026-04-05 16:09:04 +0800

    Refactor: Added data validation and numeric checks to onAdd function in ManageComponent

--- Part2-Angular/inventory-app/src/app/pages/home/home.component.ts
+++ Part2-Angular/inventory-app/src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router'; // 1. 导入 RouterLink
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink], // 2. 将其添加到 imports 数组中
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent { }
